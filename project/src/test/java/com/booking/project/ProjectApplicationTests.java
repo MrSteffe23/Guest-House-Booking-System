c8a7304b2f322a972b20c193f14d82170e81b22c
stefan <stef_stefan9@yahoo.com>
2023-03-18 17:05:46 +0200

    project structure using spring initializr

---
+++ project/src/test/java/com/booking/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
