72dc17d1e8de07a8484b971f636c2152a42aa6f8
stefan <stef_stefan9@yahoo.com>
2023-05-05 12:25:29 +0300

    UnitTest for review

--- project/src/test/java/com/booking/project/ProjectApplicationTests.java
+++ project/src/test/java/com/booking/project/ProjectApplicationTests.java
@@ -1,5 +1,8 @@
 package com.booking.project;
 
+import com.booking.project.review.Review;
+import com.booking.project.review.ReviewRepository;
+import com.booking.project.review.ReviewService;
 import com.booking.project.unitteste.*;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -19,6 +22,9 @@ class ProjectApplicationTests {
 	private InterfataDBOperations dbOperationsMock;
 	//!!!!!!!!!!!!!!!!facem mock clasele de care depinde clasa pe care vrem noi sa o testam!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	private final OperatiiDobanda operatiiDobanda = new OperatiiDobanda();
+
+	@Mock
+	private ReviewRepository reviewRepositoryMock;
 
 	@Test
 	void testDobandaMica() {
@@ -42,7 +48,7 @@ class ProjectApplicationTests {
 	}
 
 	@Test
-	void testUserRisk() {
+	void testUserRiskMedium() {
 		User user = new User("Stefan", RiskType.MEDIUM);
 		when(dbOperationsMock.getUser()).thenReturn(user);
 		OperatiiDobanda operatiiDobanda = new OperatiiDobanda(dbOperationsMock);
@@ -52,4 +58,37 @@ class ProjectApplicationTests {
 		//merita cand se trimite si un parametru in spate (la getUser())
 	}
 	//REGULA: un assert si un verify pe fiecare metoda. Daca nu (doar verify mai multe, nu assert-uri), trebuie justificat bine
+
+	@Test
+	void testUserRiskvLow() {
+		User user = new User("Stefan", RiskType.LOW);
+		when(dbOperationsMock.getUser()).thenReturn(user);
+		OperatiiDobanda operatiiDobanda = new OperatiiDobanda(dbOperationsMock);
+
+		assertEquals(operatiiDobanda.calculDobanda(), 15);
+		verify(dbOperationsMock).getUser(); //verifica ca o anumita metoda din mock s-a chemat in urma apelului de metoda
+		//merita cand se trimite si un parametru in spate (la getUser())
+	}
+
+	@Test
+	void testUserRiskHigh() {
+		User user = new User("Stefan", RiskType.HIGH);
+		when(dbOperationsMock.getUser()).thenReturn(user);
+		OperatiiDobanda operatiiDobanda = new OperatiiDobanda(dbOperationsMock);
+
+		assertEquals(operatiiDobanda.calculDobanda(), 5);
+		verify(dbOperationsMock).getUser(); //verifica ca o anumita metoda din mock s-a chemat in urma apelului de metoda
+		//merita cand se trimite si un parametru in spate (la getUser())
+	}
+
+	@Test
+	void testCreateReview() {
+		Review review = new Review(1L, 1L, 2L, "foarte frumos", 5L);
+		//when(reviewRepositoryMock.save(review)).thenReturn(review);
+		ReviewService reviewService = new ReviewService(reviewRepositoryMock);
+		reviewService.createReview(review);
+
+		verify(reviewRepositoryMock).save(review); //verifica ca o anumita metoda din mock s-a chemat in urma apelului de metoda
+		//merita cand se trimite si un parametru in spate (la getUser())
+	}
 }
