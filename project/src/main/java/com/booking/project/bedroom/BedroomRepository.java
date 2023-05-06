e2470892cb388ecd98ab6a1a191ca590887fd4ac
stefan <stef_stefan9@yahoo.com>
2023-05-06 18:32:15 +0300

    Bedroom implementation v1

--- project/src/main/java/com/booking/project/bedroom/BedroomRepository.java
+++ project/src/main/java/com/booking/project/bedroom/BedroomRepository.java
@@ -2,6 +2,8 @@ package com.booking.project.bedroom;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * This Interface is responsible for the lower level logic.<br>
  * Here are all the methods used for interacting directly with the database.
@@ -9,4 +11,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Here you can define queries, or use some predefined methods.
  */
 public interface BedroomRepository extends JpaRepository<Bedroom, Long> {
+    /**
+     *  This method is named to match the name of the idHouse field in the Bedroom class. This is because Spring Data JPA
+     *  uses naming conventions to automatically generate queries based on the names of the methods defined in the
+     *  repository interface. In this case, Spring Data JPA will generate a query that looks for bedrooms based on the
+     *  idHouse field in the Bedroom class.
+     * @param idHouse the id of the house that I need to find the bedrooms
+     * @return the list of the Bedrooms associated with a specified House by that 'idHouse'
+     */
+    List<Bedroom> findByIdHouse(Long idHouse);
+
+
+    /**
+     * This method uses Spring Data JPA's method naming convention to generate a query that searches for Bedroom
+     * entities where the number and idHouse match the given parameters
+     * @param number the number for a specific bedroom
+     * @param idHouse the id of the house a bedroom belongs
+     * @return a list of all the bedrooms in the database with a specific number and idHouse
+     */
+    List<Bedroom> findByNumberAndIdHouse(Integer number, Long idHouse);
 }
